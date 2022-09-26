c6c5bd12e8186d67cf15982548cc9ac323cc4889
Nitin Bhore <nitinbhore3@gmail.com>
2022-09-26 13:48:00 +0530

    Update text_similarity.py

--- text_similarity.py
+++ text_similarity.py
@@ -14,7 +14,6 @@ import os, re, io
 import numpy as np
 import requests
 import pandas as pd
-
 from gensim.parsing.preprocessing import remove_stopwords
 from nltk.stem import WordNetLemmatizer
 from nltk import word_tokenize
@@ -34,7 +33,7 @@ class QuestionAnswer():
         """Inits the Preprocessing"""
         self.data_df = data_df  
         self.column = column_name
-        self.processed_column_name = f"processed_{self.column}"
+        self.clean_column_name = f"clean_{self.column}"
 
     def cleanData(self, data_df, column_name):
       """Clean the data by removing stop words punctuvation"""
@@ -43,13 +42,13 @@ class QuestionAnswer():
       self.data_df[self.column] = self.data_df[self.column].apply(lambda row: re.sub(r'^\d+[.]',' ', row))    
       self.data_df[self.column] = self.data_df[self.column].apply(lambda row: re.sub(r'[^A-Za-z0-9\s]', ' ', row)) 
       for idx, question in enumerate(self.data_df[self.column]):
-        self.data_df.loc[idx, self.processed_column_name] = remove_stopwords(question)        
+        self.data_df.loc[idx, self.clean_column_name] = remove_stopwords(question)        
       return self.data_df, self.column
 
     def apply_lemmatization(self, data_df, column_name ):
         """get the root words to reduce inflection of words"""
         lemmatizer = WordNetLemmatizer()    
-        self.processed_column_name
+        self.clean_column_name
         
         for idx, question in enumerate(data_df[column_name]):
 
@@ -58,23 +57,23 @@ class QuestionAnswer():
             for word in doc:       
                 lemmatized_sentence.append(word.lemma_)      
                 ## update to the same column
-                self.data_df.loc[idx, self.processed_column_name] = " ".join(lemmatized_sentence)
+                self.data_df.loc[idx, self.clean_column_name] = " ".join(lemmatized_sentence)
 
     def run_all(self):
         data_df, column_name = self.cleanData(self.data_df, self.column)
         self.apply_lemmatization(data_df, column_name)    
         return self.data_df
 
-QA_df = pd.read_csv("COVID19_FAQ.csv")
-QA_df.head(10)
+df = pd.read_csv("COVID19_FAQ.csv")
+df.head(10)
 
 ## pre-process training question data
-text_preprocessor = QuestionAnswer(QA_df.copy(), column_name="questions")
-processed_QA_df = text_preprocessor.run_all()
-processed_QA_df.head(10)
+text_preprocessor = QuestionAnswer(df.copy(), column_name="questions")
+clean_df = text_preprocessor.run_all()
+clean_df.head(10)
 
 ## helps to retrieve similar question based of input vectors/embeddings for test query
-def retrieveSimilarFAQ(train_question_vectors, test_question_vectors, train_QA_df, train_column_name, test_QA_df, test_column_name):
+def func_get_SimilarFAQ(train_question_vectors, test_question_vectors, train_df, train_column_name, test_df, test_column_name):
     similar_question_index = []
     for test_index, test_vector in enumerate(test_question_vectors):
         sim, sim_Q_index = -1, -1
@@ -85,28 +84,28 @@ def retrieveSimilarFAQ(train_question_vectors, test_question_vectors, train_QA_d
                 sim = sim_score
                 sim_Q_index = train_index
 
-        print(f"Query Question: {test_QA_df[test_column_name].iloc[test_index]}")    
-        print(f"Retrieved Question: {train_QA_df[train_column_name].iloc[sim_Q_index]}")
+        print(f"Query Question: {test_df[test_column_name].iloc[test_index]}")    
+        print(f"Get Question: {train_df[train_column_name].iloc[sim_Q_index]}")
         print("\n")
 
-test_query_string = ["Am I considered a close contact if I was wearing a mask?",
+test_query_questions = ["Am I considered a close contact if I was wearing a mask?",
 "Is the virus that causes COVID-19 found in feces (stool)?",
 "Can the COVID-19 virus spread through sewerage systems?",
 "Should I be tested for a current infection?"]
 
-test_QA_df = pd.DataFrame(test_query_string, columns=["test_questions"])  
+test_df = pd.DataFrame(test_query_questions, columns=["test_questions"])  
 
 ## pre-process testing QA data
-text_preprocessor = QuestionAnswer(test_QA_df, column_name="test_questions")
-query_QA_df = text_preprocessor.run_all()
+text_preprocessor = QuestionAnswer(test_df, column_name="test_questions")
+query_df = text_preprocessor.run_all()
 
 ## get bert embeddings
-def get_bert_embeddings(sentences):
+def func_get_bert_embeddings(sentences):
     bert_embedding = BertEmbedding()
     return bert_embedding(sentences)
 
-query_QA_bert_embeddings_list = get_bert_embeddings(test_QA_df["test_questions"].to_list())
-question_QA_bert_embeddings_list = get_bert_embeddings(processed_QA_df["questions"].to_list())
+query_QA_bert_embeddings_list = func_get_bert_embeddings(test_df["test_questions"].to_list())
+question_QA_bert_embeddings_list = func_get_bert_embeddings(clean_df["questions"].to_list())
 
 ## store QA bert embeddings in list
 question_QA_bert_embeddings = []
@@ -118,7 +117,7 @@ query_QA_bert_embeddings = []
 for embeddings in query_QA_bert_embeddings_list:
     query_QA_bert_embeddings.append(embeddings[1])
 
-retrieveSimilarFAQ(question_QA_bert_embeddings, query_QA_bert_embeddings, processed_QA_df, "questions", query_QA_df, "test_questions")
+func_get_SimilarFAQ(question_QA_bert_embeddings, query_QA_bert_embeddings, clean_df, "questions", query_df, "test_questions")
 
 
 
