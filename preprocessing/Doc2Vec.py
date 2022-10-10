11d39695052570347c1a84a1f396a13bc76d7b98
Nitin Bhore <nitinbhore3@gmail.com>
2022-10-10 16:33:47 +0530

    Add files via upload

---
+++ preprocessing/Doc2Vec.py
@@ -0,0 +1,53 @@
+#######  Doc2Vec  #########
+
+import gensim
+import gensim.downloader as api
+"""
+Doc2vec is an NLP tool for representing documents as a vector and is a generalizing of the word2vec method.
+Args:
+  doc: List of the sentences
+Returns: 
+  model : Doc2Vec Model
+"""
+class Doc2Vec():
+  def __init__(self, doc):
+    """Inits the Preprocessing"""
+    self.doc = doc
+  
+  ######
+  def tagged_document(self, list_of_list_of_words):
+    """tagged the documents"""
+    for i, list_of_words in enumerate(list_of_list_of_words):
+        yield gensim.models.doc2vec.TaggedDocument(list_of_words, [i])
+    # data_for_training = list(tagged_document(doc))
+
+    # return data_for_training
+
+  def doc2vec_model_train(self,data_for_training):
+    """doc2vec model"""
+    # Initialise the Model
+    model = gensim.models.doc2vec.Doc2Vec(vector_size=40, min_count=2, epochs=30)
+
+    # build the vocabulary
+    model.build_vocab(data_for_training)
+
+    # train the Doc2Vec model
+    model.train(data_for_training, total_examples=model.corpus_count, epochs=model.epochs)
+
+    return model
+ 
+  def run_all(self):
+    """Run all the methods as per the requirements"""
+    # data_for_training = self.tagged_document(self.doc)
+    model = self.doc2vec_model_train(list(self.tagged_document(self.doc)))
+    return model
+
+
+#Download the Dataset
+dataset = api.load("text8")
+data = [d for d in dataset]
+
+
+doc2vec = Doc2Vec(data)
+model = doc2vec.run_all()
+model.infer_vector(['violent', 'means', 'to', 'destroy', 'the','organization'])
---
+++ preprocessing/PreprocessingClass.py
@@ -0,0 +1,43 @@
+import spacy
+"""
+Blueprint of preprocessing and lemmatization for the Natural language 
+processing
+Args:
+  doc: List of the sentences
+Returns: 
+  final_token : The list of the words
+"""
+
+class Preprocessing():
+  def __init__(self, doc):
+    """Inits the Preprocessing"""
+    self.doc = doc
+  
+  def cleanData(self, doc):
+    """Clean the data by removing stop words punctuvation"""
+
+    nlp = spacy.load('en_core_web_sm')
+    doc = doc.lower()
+    doc = nlp(doc)
+    tokens = [tokens.lower_ for tokens in doc]
+    tokens = [tokens for tokens in doc if (tokens.is_stop == False)]
+    tokens = [tokens for tokens in tokens if (tokens.is_punct == False)]
+    # print(tokens)
+    return tokens
+
+  def lemmatization(self, tokens):
+    """get the root words to reduce inflection of words"""
+      # self.tokens = tokens
+      final_token = [token.lemma_ for token in tokens]
+      # print(" ".join(final_token))
+      return final_token #" ".join(final_token)
+ 
+  def run_all(self):
+    """Run all the methods as per the requirments"""
+    tokens = self.cleanData(self.doc)
+    final_token = self.lemmatization(tokens)
+    return final_token
+
+doc = ("Find Alternatives To Pagerduty at Shopwebly, the Website to Compare Prices! Find and Compare Alternatives To Pagerduty Online. Save Now at Shopwebly! Many Products. Quick Results. Easy Access. Compare Products. Search and Discover   ")
+preprocessing = Preprocessing(doc)
+preprocessing.run_all()
